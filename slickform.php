babc5e8209af3582b4af46f2079128c468591f43
pintcoins <pintcoins@users.noreply.github.com>
2018-09-22 21:26:37 -0700

    form

--- slickform.php
+++ slickform.php
@@ -3,11 +3,11 @@
 
 if(isset($_REQUEST["isvalid"])){
 	
-	$youremail = "johnnyhopseedz@gmail.com"; // Enter your email here!!
+	$youremail = "pintcoin@gmail.com"; 
 	$usersname = $_POST["usersname"];
 	$usersemail = $_POST["usersemail"];
 	$usersphonenumber = $_POST["usersphone"];
-	$mailsubject = "Contact From Your Slickform";	
+	$mailsubject = "Contact From Pintcoins.com";	
 	$usersmessage = $_POST["userscomment"];
 	$message =
 	
